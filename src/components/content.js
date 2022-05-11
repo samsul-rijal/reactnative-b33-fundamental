a03c1c025dc37a49d1fd7d9d497d544bba16fe31
Your Name <samsulr99@gmail.com>
2022-05-11 09:41:29 +0700

    update

--- src/components/content.js
+++ src/components/content.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View,Text } from "react-native";
 
 export default function content() {
   return (
     <View>
       {/* Code Here */}
+      <Text>This is content</Text>
+      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quas cupiditate itaque ullam laborum, eum quos amet ipsa maiores perspiciatis veniam voluptatem, illum saepe exercitationem illo asperiores velit ratione laboriosam.</Text>
     </View>
   );
 }
