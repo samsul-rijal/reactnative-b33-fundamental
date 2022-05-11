18b5a98ea5db7334010d67513787f1eae85ac9d7
Your Name <samsulr99@gmail.com>
2022-05-11 09:46:20 +0700

    update

--- src/screens/embedExpression.js
+++ src/screens/embedExpression.js
@@ -1,16 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View,Text } from "react-native";
 
 export default function EmbedExpression() {
   // Create Function Here
+  function getHello(){
+    return "Batch 33"
+  }
 
   // Create a variable Here
+  const bootcamp = "Dumbways.id"
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      <Text>
+        Welcome {getHello()} di {bootcamp} 
+      </Text>
     </View>
   );
 }
