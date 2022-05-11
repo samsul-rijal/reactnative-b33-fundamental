df3706603683329b43f8da378bcef3451d13bc74
Your Name <samsulr99@gmail.com>
2022-05-11 10:38:44 +0700

    update

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,14 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View } from "react-native";
+import { View,Text } from "react-native";
 
 export default function Map() {
   // Create Dummy Data With Array
+  const socialMedia = ['Instagram', 'Facebook', 'Twitter', 'Tiktok']
 
   return (
     <View>
       <StatusBar />
       {/* Code Here */}
+      {socialMedia.map((item)=>(
+        <Text>{item}</Text>
+      ))}
+
+      {/* {socialMedia.map((item)=>{
+        return(
+          <Text>{item}</Text>
+        )
+      })} */}
     </View>
   );
 }
