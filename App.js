d9ef502d6b30fa5fe1b1d0a3a2195b36dec86159
Your Name <samsulr99@gmail.com>
2022-05-11 11:46:52 +0700

    update

--- App.js
+++ App.js
@@ -3,18 +3,71 @@ import React from "react";
 import { NativeBaseProvider, extendTheme } from "native-base";
 
 // Import font with Expo
+import {
+  BalsamiqSans_400Regular,
+  BalsamiqSans_400Regular_Italic,
+  useFonts,
+} from '@expo-google-fonts/balsamiq-sans'
+import AppLoading from "expo-app-loading";
 
 // Import Container
+import Container from './Container'
 
 export default function App() {
   // Load Font with Expo
+  let [fonstLoaded] = useFonts({
+    BalsamiqSans_400Regular,
+    BalsamiqSans_400Regular_Italic
+  })
 
   // Setup Font
+  const fontConfig = {
+    BalsamiqSans: {
+      400: {
+        normal: 'BalsamiqSans_400Regular',
+        italic: 'BalsamiqSans_400Regular_Italic'
+      }
+    }
+  }
 
   // Setup Theme
+  const customeColor =  {
+    primary: {
+      50: '#ecfeff',
+      100: '#cffafe',
+      200: '#a5f3fc',
+      300: '#67e8f9',
+      400: '#22d3ee',
+      500: '#06b6d4',
+      600: '#0891b2',
+      700: '#0e7490',
+      800: '#155e75',
+      900: '#164e63',
+    }
+  }
 
   // Configuration Native Base Custom Theme
-  return (
-    // Setup Provider
-  );
+  const theme = extendTheme({
+    colors: customeColor,
+    fontConfig,
+    font: {
+      heading: "BalsamiqSans",
+      body: "BalsamiqSans",
+      mono: "BalsamiqSans",
+    },
+    config: {
+      initialColorMode: 'dark'
+    }
+  })
+
+  if(!fonstLoaded){
+    return <AppLoading />
+  } else {
+    return (
+      // Setup Provider
+      <NativeBaseProvider theme={theme}>
+        <Container />
+      </NativeBaseProvider>
+    );
+  }
 }
--- src/screens/formNativeBase.js
+++ src/screens/formNativeBase.js
@@ -18,5 +18,50 @@ import {
 } from "native-base";
 
 export default function FormNativeBase() {
-  return <Box>{/* Code Here */}</Box>;
+  return(
+    <Box 
+      safeArea
+      bg="primary.100"
+      flex={1}
+      p={10}
+      w='100%'
+      mx='auto'
+      justifyContent='center'
+    >
+
+      <Heading color="primary.900" size='lg'>
+        Welcome
+      </Heading>
+
+      <Heading color="primary.900" size='xs'>
+        Sign in to continue!
+      </Heading>
+
+      <VStack space={2} mt={5}>
+        <FormControl>
+          <FormControl.Label _text={{color: 'primary.600', fontWeight: 'bold',fontSize:'sm'}}>
+            Email
+          </FormControl.Label>
+          <Input />
+        </FormControl>
+
+        <FormControl>
+          <FormControl.Label _text={{color: 'primary.600', fontWeight: 'bold',fontSize:'sm'}}>
+            Password
+          </FormControl.Label>
+          <Input type="password" />
+
+          <Link _text={{fontSize: 'xs', fontWeight: '700', color: 'primary.600'}}>
+            Forgot Password?
+          </Link>
+        </FormControl>
+      </VStack>
+
+      <VStack space={2} mt={5}>
+        <Button colorScheme="primary" _text={{color: 'white'}}>
+          Login
+        </Button>
+      </VStack>
+    </Box>
+  )
 }
